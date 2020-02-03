4a1dfd20b671e2b30f00588381eb6c3e3f94aa52
Nathan Kennedy <nathankennedy122@gmail.com>
2020-02-03 17:33:26 -0500

    Image Processing
    Wrote a function to process images into multi-dimenstional arrays. Started working on a function to pull files from a folder and feed them into the image processor function.

---
+++ ImageProcessingTest.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+path = 'data/00000001_000.png'
+
+temppath = 'data/'
+
+img_list = []
+
+#function that appends the name of all files in a folder to a list
+def feed_function():
+    img_list = [f for f in listdir(temppath) if isfile(join(temppath, f))]
+
+    for x in img_list:
+        print(x)
+        #add the file name to temppath
+        #feed that new string into load_image()
+
+#function that processes each image based on the pixels - three dimensional array.
+def load_image(path):
+
+    # (width, height, channels) < the order of the three dimensional array
+
+    with Image.open(path) as image:
+        im_arr = np.frombuffer(image.tobytes(), dtype=np.uint8)
+        im_arr = im_arr.reshape((image.size[1], image.size[0], -1)) # this line reshapes the one dimensional array into an array of arrays
+
+    return im_arr
+
+images = load_image(path)
+
+#print(images)
+
+#print(len(images))
+
+print(feed_function())
