64c37053290e24893c5ccf74106849ee406c5757
Justice Suh <justice.suh@gmail.com>
2020-02-23 21:29:02 -0500

    Load dataset

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,11 @@
+import tensorflow as tf
+
+def load_dataset(name):
+    filenames, labels = load_data(name)
+    dataset = tf.data.Dataset.from_tensor_slices((tf.constant(filenames), tf.constant(labels)))
+    dataset = dataset.map(parse_image)
+    return dataset
+
 def load_data(name):
     mapping = {}
     with open('data/{}.txt'.format(name), 'r') as f:
@@ -14,11 +22,16 @@ def load_data(name):
     filenames = []
     labels = []
     for filename, label in mapping.items():
-        filenames.append(filename)
+        filenames.append('data/{}/{}'.format(name, filename))
         labels.append(label)
     
     return filenames, labels
 
+def parse_image(filename, label):
+    image = tf.io.read_file(filename)
+    image = tf.io.decode_png(image)
+    image = tf.image.resize(image, (1024, 1024))
+    return image, label
+
 if __name__ == '__main__':
-    train_filenames, train_labels = load_data('training')
-    print(train_filenames, train_labels)
+    train_dataset = load_dataset('training')
